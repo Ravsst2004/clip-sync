74e749bb1b66ebc9dbf7e393d4ac8d0da9fc0843
Ravsst2004 <ravindraviswanatha@gmail.com>
2024-10-23 19:37:46 +0800

    add animation on view

--- src/components/ui/FeaturesList.jsx
+++ src/components/ui/FeaturesList.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import { useInView, motion } from "framer-motion";
+import { useRef } from "react";
 
 const FeaturesList = ({ title, description }) => {
+  const scope = useRef(null);
+  const isInView = useInView(scope, { once: true });
+
   return (
-    <div>
+    <motion.div
+      ref={scope}
+      animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : 100 }}
+      transition={{ duration: 2 }}
+    >
       <h5 className="mb-2 text-2xl font-bold text-darkGrayishBlue">{title}</h5>
       <p className="max-w-md text-grayishBlue">{description}</p>
-    </div>
+    </motion.div>
   );
 };
 
